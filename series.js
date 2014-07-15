776959f98ccc2bce3135eff01fafb3ef41fb3871
Alexey Karpov <karpovad@yandex.ru>
2014-07-16 01:40:21 +0400

    Легкое решение проблемы self.

--- series.js
+++ series.js
@@ -1,5 +1,7 @@
 (function () {
-    var plot = new app.Plotter("graph", {planeBorder: [-5, 5, -5, 5]});
+    app.Plotter1 = app.Plotter({});
+    var plot = new app.Plotter1("graph", {planeBorder: [-5, 5, -5, 5]});
+
     var point2 = plot.addPoint(0, 0, { movable: true });
     var point = plot.addPoint(2, 2, { movable: false });
     plot.removePoint(point.getNumber());
